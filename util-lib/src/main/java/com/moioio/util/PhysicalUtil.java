f529b4dccb11f6bf01ecc7d96403360c9545138d
Moioio <tianbocvfe@gmail.com>
2020-11-25 19:52:39 +0800

    update ui

--- util-lib/src/main/java/com/moioio/util/PhysicalUtil.java
+++ util-lib/src/main/java/com/moioio/util/PhysicalUtil.java
@@ -71,4 +71,23 @@ public class PhysicalUtil {
     }
 
 
+    public static boolean circleWithPoint(float rx,float ry,float r,float x,float y)
+    {
+        boolean isOk = false;
+
+        float a = (float) Math.pow(Math.abs(x - rx), 2);
+        float b = (float) Math.pow(Math.abs(y - ry), 2);
+
+//        MyLog.debug("a----:"+a);
+//        MyLog.debug("b----:"+b);
+//        MyLog.debug("Math.sqrt( a+b )----:"+Math.sqrt( a+b ));
+
+
+        if (Math.sqrt( a+b )<= r) {
+            isOk = true;
+        }
+        return isOk;
+    }
+
+
 }
