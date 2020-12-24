e6d83d599e37ca1d1e674cb7da37b00d771ac847
Moioio <tianbocvfe@gmail.com>
2020-12-24 18:09:01 +0800

    update ui

--- easy-ui/src/main/java/com/moioio/android/easyui/MyActivity.java
+++ easy-ui/src/main/java/com/moioio/android/easyui/MyActivity.java
@@ -28,6 +28,28 @@ public class MyActivity extends Activity {
 
 
 
+    public void setNavigationBarColor(int color)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            getWindow().setNavigationBarColor(color);
+        }
+    }
+
+    public void setNavigationBarStatusBarTranslucent(){
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+
+
     public void setWindowStatusBarTrans() {
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
