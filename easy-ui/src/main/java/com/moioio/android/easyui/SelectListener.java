b61a9edd7ea2b2ae12e4d0402a5fec3c0de28581
Moioio <tianbocvfe@gmail.com>
2021-02-09 02:17:31 +0800

    update ui

---
+++ easy-ui/src/main/java/com/moioio/android/easyui/SelectListener.java
@@ -0,0 +1,10 @@
+package com.moioio.android.easyui;
+
+import android.content.Context;
+
+public interface SelectListener {
+
+    void onSelect(Context context,Object obj);
+    void onCancel(Context context);
+
+}
